2f3071f0c24d78f52ac09682a9eb1111d916d164
Pranav Rajesh Jadhav <iamoutput09@gmail.com>
2024-01-11 10:29:56 +0530

    setup: 📦 script file

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+///////////////////////
+// Elements
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form-input--type');
+const inputDistance = document.querySelector('.form-input--distance');
+const inputDuration = document.querySelector('.form-input--duration');
+const inputCadance = document.querySelector('.form-input--cadance');
+const inputElevation = document.querySelector('.form-input--elevation');
