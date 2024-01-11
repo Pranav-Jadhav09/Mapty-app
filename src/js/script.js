bc89b97f20815eb2d27494d38ee798da66c53b32
Pranav Rajesh Jadhav <iamoutput09@gmail.com>
2024-01-11 12:09:18 +0530

    feat: ✨ display map init

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 ///////////////////////
 // Elements
 const form = document.querySelector('.form');
@@ -7,3 +9,43 @@ const inputDistance = document.querySelector('.form-input--distance');
 const inputDuration = document.querySelector('.form-input--duration');
 const inputCadance = document.querySelector('.form-input--cadance');
 const inputElevation = document.querySelector('.form-input--elevation');
+
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(latitude, longitude);
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+      const map = L.map('map').setView([latitude, longitude], 13);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker([latitude, longitude])
+        .addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
+    },
+    function () {
+      alert('Could Not Get Your Position!');
+    }
+  );
