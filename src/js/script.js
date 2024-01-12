4fffea5161f703f42cbdfaa678e9ec10589766e3
Pranav Rajesh Jadhav <iamoutput09@gmail.com>
2024-01-12 11:15:02 +0530

    code: 🚀 render marker onclick map

--- src/js/script.js
+++ src/js/script.js
@@ -2,14 +2,6 @@
 
 ///////////////////////
 // Elements
-const form = document.querySelector('.form');
-const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form-input--type');
-const inputDistance = document.querySelector('.form-input--distance');
-const inputDuration = document.querySelector('.form-input--duration');
-const inputCadance = document.querySelector('.form-input--cadance');
-const inputElevation = document.querySelector('.form-input--elevation');
-
 const months = [
   'January',
   'February',
@@ -25,25 +17,47 @@ const months = [
   'December',
 ];
 
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form-input--type');
+const inputDistance = document.querySelector('.form-input--distance');
+const inputDuration = document.querySelector('.form-input--duration');
+const inputCadance = document.querySelector('.form-input--cadance');
+const inputElevation = document.querySelector('.form-input--elevation');
+
+let map, mapEvent;
+
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
     function (position) {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
-      console.log(latitude, longitude);
-      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
-
       const coords = [latitude, longitude];
 
-      const map = L.map('map').setView(coords, 14);
+      map = L.map('map').setView(coords, 13);
 
       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      map.on('click', function (mapEvent) {
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup(
+          L.popup({
+            autoClose: false,
+            closeOnClick: false,
+            className: 'cycling-popup',
+          })
+        )
+        .setPopupContent(`Out Side click function`)
+        .openPopup();
+
+      // Handling Clicks on map
+      map.on('click', function (mapEv) {
+        mapEvent = mapEv;
         const { lat, lng } = mapEvent.latlng;
+
         L.marker([lat, lng])
           .addTo(map)
           .bindPopup(
